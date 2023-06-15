8d917d7bdb75f23b9e4574c3b7b74294b94df169
Daniel C. Perez <54647617+DanielxDeveloper@users.noreply.github.com>
2023-06-14 23:50:17 -0500

    Create index.js

---
+++ asimetrico/index.js
@@ -0,0 +1,29 @@
+// class keys {
+
+//     create_publicKeys(user){
+//         //code
+//         u_nombre = user['nombre'];
+//         u_apellido = user['apellido']
+//         u_edad = user['edad']
+//         u_sexo = user['sexo']
+//         return 0;
+//     }
+
+//     create_privatekeys(){
+//         //code
+//         return 0;
+//     }
+// }
+
+// Generar un número aleatorio entre 0 y 99
+const randomNumbers = new BigInt64Array(5); // Crear un array de bytes para almacenar el número aleatorio
+crypto.getRandomValues(randomArray); // Llenar el array con valores aleatorios
+console.log(randomNumbers)
+
+randomNumbers.forEach(random => {
+    random = random
+});
+// const randomNumber = randomArray[0] % 100; // Obtener un número entre 0 y 99
+
+// console.log(randomNumber); // Mostrar el número aleatorio
+// console.log('perro')
